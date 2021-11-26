3ee26855eef501d03ff69e70782c374b416b7f7c
Abhiram <abhiram.satha@gmail.com>
2021-11-26 01:59:42 +0000

    Added the Submit form

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs')
 
 const urlDatabase = {
@@ -10,15 +13,25 @@ const urlDatabase = {
 };
 
 
+
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render('urls_show', templateVars)
-})
+  })
+
+  app.post('/urls', (req, res) => {
+    console.log(req.body);
+    res.send('OK');
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -41,3 +54,9 @@ app.get('/set', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+
+};
+
+generateRandomString();
